8f1634ae470626793f58c9897c08e37acc82a8b5
neha123f <121794908+neha123f@users.noreply.github.com>
2023-10-04 17:03:51 +0530

    Update count_severity.sh

--- count_severity.sh
+++ count_severity.sh
@@ -12,8 +12,8 @@ json_file="$1"
 
 # Use jq to count and format severity levels
 cat "$json_file" | jq '[.. | select(.severity?)] | group_by(.severity) | map({severity: .[0].severity, count: length})'
-#trafficlight_value=$(cat "$json_file" | jq '. | select(.trafficLight != null).trafficLight')
-#echo  "Trafficlight:$trafficlight_value"
+trafficlight_value=$(cat "$json_file" | jq '. | select(.trafficLight != null).trafficLight')
+echo  "Trafficlight:$trafficlight_value"
 
 
 
