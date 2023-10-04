55fba60e9e0b0f6247e62247552458f7fcd029eb
neha123f <121794908+neha123f@users.noreply.github.com>
2023-10-04 17:30:00 +0530

    Update count_severity.sh

--- count_severity.sh
+++ count_severity.sh
@@ -10,9 +10,13 @@ fi
 json_file="$1"
 
 # Use jq to count and format severity levels
-cat "$json_file" | jq '[.. | select(.severity?)] | group_by(.severity) | map({severity: .[0].severity, count: length})'
+severity_count=$(cat "$json_file" | jq '[.. | select(.severity?)] | group_by(.severity) | map({severity: .[0].severity, count: length})')
 
 # Use jq to extract the "Trafficlight" value (correct capitalization)
 trafficlight_value=$(cat "$json_file" | jq -r '.trafficLight')
 
-echo  "Trafficlight: $trafficlight_value"
+# Create a JSON object containing severity count and traffic light
+result_json="{\"SeverityCount\": $severity_count, \"Trafficlight\": \"$trafficlight_value\"}"
+
+# Print the JSON result
+echo "$result_json"
