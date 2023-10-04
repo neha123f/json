df455bb9d2eaab3b6335c88c369296cc16296099
neha123f <121794908+neha123f@users.noreply.github.com>
2023-10-04 14:16:07 +0530

    Add files via upload

---
+++ count_severity.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if a filename is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <json_file>"
+  exit 1
+fi
+
+# Get the JSON filename from the command line argument
+json_file="$1"
+
+
+# Use jq to count and format severity levels
+cat "$json_file" | jq '[.. | select(.severity?)] | group_by(.severity) | map({severity: .[0].severity, count: length})'
+
+
