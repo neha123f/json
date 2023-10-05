e041c5deb0dac4f3b3b1076a7b7264e1c14a2df3
neha123f <121794908+neha123f@users.noreply.github.com>
2023-10-05 11:35:50 +0530

    Update count_severity.sh

--- count_severity.sh
+++ count_severity.sh
@@ -6,7 +6,7 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-# Get the JSON filename from the command line 
+# Get the JSON filename from the cmd
 json_file="$1"
 
 # Use jq to count and format severity levels
